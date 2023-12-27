6e167d5fb0355f877046de11d2492dae8a307cef
Hart Liddell <hart@leapingfish.io>
2023-12-27 15:47:51 -0500

    Add form fields

--- frontend/src/components/UpdateCraftForm.vue
+++ frontend/src/components/UpdateCraftForm.vue
@@ -1,17 +1,40 @@
 <script setup>
-defineProps({
+import { reactive } from 'vue';
+const props = defineProps({
   craft: Object
 });
 
 const emit = defineEmits(['close']);
+const updateFields = reactive({ ...props.craft })
 </script>
 
 <template>
+  <form class="formFields">
+    <label for="craftName">Name of craft:
+      <input id="craftName" v-model="updateFields.name" />
+    </label>
+    <label for="craftType">Type of craft:
+      <input id="craftType" v-model="updateFields.type" />
+    </label>
+    <label for="craftBrand">Brand of craft:
+      <input id="craftBrand" v-model="updateFields.brand" />
+    </label>
+    <label for="craftAge">Age of craft:
+      <input id="craftAge" v-model="updateFields.age" />
+    </label>
+    <label for="craftPrice">Price of craft:
+      <input id="craftPrice" v-model="updateFields.price" />
+    </label>
+  </form>
   <button @click="emit('close')">Cancel</button>
   <button>Update</button>
-  <br />
-  <div>{{ JSON.stringify(craft) }}</div>
 </template>
 
 <style scoped>
+form {
+  padding: 1rem;
+}
+label {
+  display: block;
+}
 </style>
