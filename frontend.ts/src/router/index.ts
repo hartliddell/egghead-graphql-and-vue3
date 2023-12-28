76a2943596249cfed3ec766de4150ffb0c97f7a5
Hart Liddell <hart@leapingfish.io>
2023-12-28 11:31:29 -0500

    Update main and add router

---
+++ frontend.ts/src/router/index.ts
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from "vue-router";
+import CraftList from "../components/CraftList.vue";
+import About from "../components/About.vue";
+import Craft from "../components/Craft.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: CraftList,
+  },
+  {
+    path: "/about",
+    component: About,
+  },
+  {
+    path: "/crafts/:id",
+    component: Craft,
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
