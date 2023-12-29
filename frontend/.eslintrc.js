a818d6389cead613c7d9ef3ef36c75744504c900
Hart Liddell <hart@leapingfish.io>
2023-12-29 16:16:37 -0600

    Update ruleset

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -12,5 +12,6 @@ module.exports = {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
     'vue/multi-word-component-names': 'off',
+    'vue/require-default-prop': 'off',
   }
 }
